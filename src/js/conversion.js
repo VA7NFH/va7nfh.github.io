7e1dde9de3b4f0ae2fa641b823cc89711378ee6e
StealthHydra179 <aidenm888@gmail.com>
2021-03-02 11:19:44 -0500

    pain

--- src/js/conversion.js
+++ src/js/conversion.js
@@ -1,8 +1,43 @@
+var currencyRates = {
+        "USD": 0.8,
+        "GBP": 0.6
 
-var currentlySelectd = "CAD";
+}
+var currentlySelected = "CAD";
 var valueInCad = 0;
 
 var product = document.getElementById("product");
-if (product == "hummingbird") {
+if (product.textContent === "hummingbird") {
     valueInCad = 4000;
+}
+
+var currentPos = {
+    "1": "USD",
+    "2": "GBP"
+}
+
+var order = ["CAD", "USD", "GBP"]
+var currentVal = valueInCad;
+
+function convert(clickedID) {
+    currentlySelected = currentPos[clickedID];
+    currentVal = valueInCad*currencyRates[currentlySelected];
+
+
+    //upade display list and current pos lists
+    var currentEdited = 0;
+    var button = document.querySelector('#buttonTitle');
+        button.textContent = currentlySelected;
+    for (var i = 0; i < order.length; i++) {
+        if (order[i] == currentlySelected) {
+            continue;
+        }
+        currentPos[(currentEdited+1).toString()] = order[i];
+        document.getElementById((currentEdited+1).toString()).textContent = order[i];
+        currentEdited++;
+    }
+
+
+    //update curernt displayed value
+
 }
